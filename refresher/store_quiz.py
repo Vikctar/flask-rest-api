adfdde02c452c2d264e11579cdf6045a6925ae3a
Vikctar <vikc.steve@gmail.com>
2018-05-04 23:11:49 +0300

    Setup the quiz

---
+++ refresher/store_quiz.py
@@ -0,0 +1,23 @@
+"""This coding exercise requires you to complete an implementation
+of three methods of a class.
+1. The __init__ method, which should take an argument, name. It should initialize
+self.name to be the argument, and self.items to be an empty list
+2. The add_item method, which should append a dictionary representing an item to the store's items property.
+The dictionary should have keys name and price.
+3. The stock_price method, which should add up each item price inside self.items to come up with a total
+and return that."""
+
+
+class Store:
+    def __init__(self):
+
+    # You'll need 'name' as an argument to this method.
+    # Then, initialise 'self.name' to be the argument, and 'self.items' to be an empty list.
+
+
+    def add_item(self, name, price):
+
+    # Create a dictionary with keys name and price, and append that to self.items.
+
+    def stock_price(self):
+# Add together all item prices in self.items and return the total.
