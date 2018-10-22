d81d3d542a8597413c5f5cf2d42bb46b930878d2
Vikctar <vikc.steve@gmail.com>
2018-10-22 09:21:35 +0300

    **kwargs aka key word arguments

--- refresher/inheritance.py
+++ refresher/inheritance.py
@@ -8,18 +8,18 @@ class Student:
         return sum(self.marks) / len(self.marks)
 
     @classmethod
-    def friend(cls, origin, friend_name, *args):
-        return cls(friend_name, origin.school, *args)
+    def friend(cls, origin, friend_name, **kwargs):
+        return cls(friend_name, origin.school, **kwargs)
 
 
 class WorkingStudent(Student):
-    def __init__(self, name, school, salary):
+    def __init__(self, name, school, salary, job_title):
         super().__init__(name, school)
         self.salary = salary
 
 
-james = WorkingStudent("James", "Pathways Academy", 20000)
+james = WorkingStudent("James", "Pathways Academy", 20000, "Software Developer")
 
-barry = WorkingStudent.friend(james, "Barry", 17000)
+barry = WorkingStudent.friend(james, "Barry", salary=17000, job_title='Software Developer')
 print(barry.school)
 print(james.salary)
