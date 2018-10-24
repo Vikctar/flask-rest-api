5fc577b4f95442fa2e913e367dba781bef6706cc
Vikctar <vikc.steve@gmail.com>
2018-10-24 16:21:28 +0300

    advanced decorators...man did i mention i do not know what am doing *sigh

--- refresher/decorators.py
+++ refresher/decorators.py
@@ -13,12 +13,36 @@ def my_decorator(func):
     return function_that_runs_func()
 
 
-@my_decorator
+# @my_decorator
 def my_function():
     print("I'm in the function!")
 
+    #
+    # try:
+    #     my_function()
+    # except TypeError:
+    #     pass
 
-try:
-    my_function()
-except TypeError:
-    pass
+
+def decorator_with_arguments(number):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def function_that_runs_func(*args, **kwargs):
+            print("In the decorator")
+            if number == 56:
+                print("Not running the function!")
+            else:
+                func(*args, **kwargs)
+            print("After the decorator!")
+
+        return function_that_runs_func()
+
+    return my_decorator
+
+
+@decorator_with_arguments(58)
+def my_function_too(x, y):
+    print(x + y)
+
+
+my_function_too(57, 67)
