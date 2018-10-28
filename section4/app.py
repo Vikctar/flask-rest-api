16af9d4495b34c69903fc01b21ecf0b687816680
Vikctar <vikc.steve@gmail.com>
2018-10-28 12:21:35 +0300

    creating a resource

--- section4/app.py
+++ section4/app.py
@@ -4,12 +4,27 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+items = []
+
 
 class Student(Resource):
     def get(self, name):
         return {'student': name.capitalize()}
 
 
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'].lower() == name.lower():
+                return item
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item
+
+
 api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
 
 app.run(port=5000, debug=True)
